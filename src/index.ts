32c7b1024bf83d3683db8d07c91b504118ecd135
tranducminh <tranfminh@gmail.com>
2022-08-05 15:13:41 +0700

    firebase push notification

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import config from './Config';
 import RequestHandler from './consumer/RequestHandler';
 import { Logger, Kafka } from 'common';
 import { Container } from 'typedi';
+import { readFileSync } from 'fs';
+import admin from 'firebase-admin';
 
 Logger.create(config.logger.config, true);
 Logger.info('Starting...');
@@ -14,16 +16,21 @@ async function init() {
         true,
         null,
         {
-          serviceName: config.clusterId,
-          nodeId: config.nodeId,
+            serviceName: config.clusterId,
+            nodeId: config.nodeId,
         },
         config.kafkaProducerOptions,
         {},
         config.kafkaConsumerOptions,
-        {},
-      );
+        {}
+    );
     const requestHandler = Container.get(RequestHandler);
     requestHandler.init();
+    const serviceAccount = JSON.parse(readFileSync(config.firebase.authKey, 'utf-8'));
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: config.firebase.databaseURL,
+    });
 }
 
 init()
