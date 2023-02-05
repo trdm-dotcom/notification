8f429e5d3e432f8598be98903272f75eaa0df2bd
Duc Minh <tranfminh@gmail.com>
2023-02-05 09:56:06 +0700

    fix use promise all

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ Logger.info('Starting...');
 function init() {
     try {
         Logger.info('run service notification');
-        mongoose.set("strictQuery", false);
+        mongoose.set('strictQuery', false);
         mongoose.connect(config.mongo.url, config.mongo.options).then(() => {
             Logger.info('connected to mongo!');
         });
@@ -37,21 +37,34 @@ function init() {
             {}
         );
         Promise.all([
-            createService(Kafka.getInstance(), {
-                serviceName: config.clusterId,
-                nodeId: config.clientId,
-                listeningTopic: config.topic.manager_notification,
+            new Promise((resolve: (value: unknown) => void, reject: (reason?: any) => void) => {
+                createService(Kafka.getInstance(), {
+                    serviceName: config.clusterId,
+                    nodeId: config.clientId,
+                    listeningTopic: config.topic.manager_notification,
+                });
+                resolve(`createService ${config.topic.manager_notification}`);
             }),
-            createService(Kafka.getInstance(), {
-                serviceName: config.clusterId,
-                nodeId: config.clientId,
-                listeningTopic: config.topic.push_notification,
+            new Promise((resolve: (value: unknown) => void, reject: (reason?: any) => void) => {
+                createService(Kafka.getInstance(), {
+                    serviceName: config.clusterId,
+                    nodeId: config.clientId,
+                    listeningTopic: config.topic.push_notification,
+                });
+                resolve(`createService ${config.topic.push_notification}`);
             }),
-        ]);
-        Promise.all([
-            Container.get(RequestHandler).init(),
-            Container.get(ManangerNotificationHandler).init(),
-            Container.get(PushNotificationHandler).init(),
+            new Promise((resolve: (value: unknown) => void, reject: (reason?: any) => void) => {
+                Container.get(RequestHandler).init();
+                resolve(`init container RequestHandler`);
+            }),
+            new Promise((resolve: (value: unknown) => void, reject: (reason?: any) => void) => {
+                Container.get(ManangerNotificationHandler).init();
+                resolve(`init container ManangerNotificationHandler`);
+            }),
+            new Promise((resolve: (value: unknown) => void, reject: (reason?: any) => void) => {
+                Container.get(PushNotificationHandler).init()
+                resolve(`init container PushNotificationHandler`);
+            }),
         ]);
     } catch (error) {
         Logger.error(error);
