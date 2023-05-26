cc2089b7ec6c7fdf9086e68a01793d5caf234285
Duc Minh <tranfminh@gmail.com>
2023-05-26 04:23:03 +0000

    log message request

--- src/consumer/RequestHandler.ts
+++ src/consumer/RequestHandler.ts
@@ -1,5 +1,5 @@
 import { Inject, Service } from 'typedi';
-import { Errors, Kafka, NotificationMessage, MethodEnum, Logger, Utils} from 'common';
+import { Errors, Kafka, NotificationMessage, MethodEnum, Logger, Utils } from 'common';
 import config from '../Config';
 import SmsService from '../service/SmsService';
 import EmailService from '../service/EmailService';
@@ -8,49 +8,49 @@ import { FirebaseService } from '../service/FirebaseService';
 
 @Service()
 export default class RequestHandler {
-    @Inject()
-    private smsService: SmsService;
+  @Inject()
+  private smsService: SmsService;
 
-    @Inject()
-    private emailService: EmailService;
+  @Inject()
+  private emailService: EmailService;
 
-    @Inject()
-    private firebaseService: FirebaseService;
+  @Inject()
+  private firebaseService: FirebaseService;
 
-    public init() {
-        const handle: Kafka.KafkaRequestHandler = new Kafka.KafkaRequestHandler(Kafka.getInstance());
-        Kafka.createConsumer(
-            config,
-            config.kafkaConsumerOptions,
-            config.requestHandlerTopics,
-            (message: Kafka.IKafkaMessage) => handle.handle(message, this.handleRequest),
-            config.kafkaTopicOptions
-        );
+  public init() {
+    const handle: Kafka.KafkaRequestHandler = new Kafka.KafkaRequestHandler(Kafka.getInstance());
+    Kafka.createConsumer(
+      config,
+      config.kafkaConsumerOptions,
+      config.requestHandlerTopics,
+      (message: Kafka.IKafkaMessage) => handle.handle(message, this.handleRequest),
+      config.kafkaTopicOptions
+    );
+  }
+
+  private handleRequest: Kafka.Handle = async (message: Kafka.IMessage) => {
+    Logger.info(`Endpoint received message: ${JSON.stringify(message)}`);
+    const jsonParser = new JsonParser();
+    if (message == null || message.data == null) {
+      return Promise.reject(new Errors.SystemError());
+    } else {
+      let notificationMessage: NotificationMessage = jsonParser.transform(message.data, {
+        mainCreator: () => [NotificationMessage],
+      });
+      let invalidParams = new Errors.InvalidParameterError();
+      Utils.validate(notificationMessage.getTemplate(), 'template')
+        .setRequire()
+        .throwValid(invalidParams);
+      invalidParams.throwErr();
+      switch (notificationMessage.getMethod()) {
+        case MethodEnum.EMAIL:
+          return this.emailService.sendEmail(notificationMessage, message.transactionId);
+        case MethodEnum.SMS:
+          return this.smsService.sendSms(notificationMessage, message.transactionId);
+        case MethodEnum.FIREBASE:
+          return this.firebaseService.pushMessage(notificationMessage, message.transactionId);
+      }
+      return false;
     }
-
-    private handleRequest: Kafka.Handle = async (message: Kafka.IMessage) => {
-        const jsonParser = new JsonParser();
-        if (message == null || message.data == null) {
-            return Promise.reject(new Errors.SystemError());
-        } else {
-            Logger.info(`Endpoint received message: ${JSON.stringify(message)}`, message);
-            let notificationMessage: NotificationMessage = jsonParser.transform(message.data, {
-                mainCreator: () => [NotificationMessage]
-            });       
-            let invalidParams = new Errors.InvalidParameterError();
-            Utils.validate(notificationMessage.getTemplate(), 'template')
-                .setRequire()
-                .throwValid(invalidParams);
-            invalidParams.throwErr();
-            switch(notificationMessage.getMethod()){
-                case MethodEnum.EMAIL:
-                    return this.emailService.sendEmail(notificationMessage, message.transactionId);
-                case MethodEnum.SMS:
-                    return this.smsService.sendSms(notificationMessage, message.transactionId);
-                case MethodEnum.FIREBASE:
-                    return this.firebaseService.pushMessage(notificationMessage, message.transactionId);
-            }
-            return false;
-        }
-    };
+  };
 }
